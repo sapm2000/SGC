5b1f3c12f888060152bcb6f97026361923e7c3a2
Anthony Jhen <ajhensuarez@gmail.com>
2020-03-27 22:41:32 -0400

    22
    2222

---
+++ CrudElectiva/src/modelo/ModeloWhile.java
@@ -0,0 +1,34 @@
+package modelo;
+
+import javax.swing.JOptionPane;
+
+
+public class ModeloWhile {
+    
+    public int numeroA;
+
+    public int getNumeroA() {
+        return numeroA;
+    }
+
+    public void setNumeroA(int numeroA) {
+        this.numeroA = numeroA;
+    }
+    
+public int whiledo(){
+    
+       if (this.numeroA <= 15) {
+           int i = 0;
+                         do {        
+               
+                  JOptionPane.showMessageDialog(null,%i.\n);
+                    i++;
+    } while (i<=0);
+           return 0;
+       }else{
+           JOptionPane.showMessageDialog(null, "debe introducir un numero menor que 15");
+       }
+ return 0;
+}    
+    
+}
