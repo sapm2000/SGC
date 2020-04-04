c17529ca4f73e5d6cd4fe1513a0951da5075f8d0
Anthony Jhen <ajhensuarez@gmail.com>
2020-04-04 00:10:20 -0400

    buenas noches mi rey
    besitos

--- Bitacora/Nueva Carpeta/SGC/src/controlador/controladorBanco.java
+++ Bitacora/Nueva Carpeta/SGC/src/controlador/controladorBanco.java
@@ -8,9 +8,15 @@ package controlador;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.RowFilter;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 import vista.banco;
 import vista.catalogoBanco;
 
@@ -18,10 +24,11 @@ import vista.catalogoBanco;
  *
  * @author rma
  */
-public class controladorBanco implements ActionListener, MouseListener{
+public class controladorBanco implements ActionListener, MouseListener, KeyListener{
 
     private banco ban;
     private catalogoBanco cban;
+    DefaultTableModel dm;
 
     public controladorBanco(banco ban, catalogoBanco cban) {
         this.ban = ban;
@@ -34,12 +41,15 @@ public class controladorBanco implements ActionListener, MouseListener{
         this.ban.btnLimpiar.addActionListener(this);
         this.ban.btnModificar.addActionListener(this);
         this.cban.jTable1.addMouseListener(this);
+        this.cban.jTable1.addKeyListener(this);
+        this.cban.jTextField1.addKeyListener(this);
 
     }
 
     public void actionPerformed(ActionEvent e) {
         
         if (e.getSource() == cban.jButton1) {
+
             int botonDialogo = JOptionPane.YES_NO_OPTION;
             int result = JOptionPane.showConfirmDialog(null, "ENCONTRO EL REGISTRO?", "REGISTRO", botonDialogo);
             if (result == 0) {
@@ -127,4 +137,30 @@ public class controladorBanco implements ActionListener, MouseListener{
     public void mouseExited(MouseEvent e) {
         
     }
+    
+    
+        private void filtro(String consulta, JTable jtableBuscar){
+        dm = (DefaultTableModel) jtableBuscar.getModel();
+        TableRowSorter<DefaultTableModel> tr = new TableRowSorter<>(dm);
+        jtableBuscar.setRowSorter(tr);
+        tr.setRowFilter(RowFilter.regexFilter(consulta));
+}
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        filtro(cban.jTextField1.getText(), cban.jTable1);
+        
+    }
+    
 }
