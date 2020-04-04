ce7a27d741c3afa9491993a99fa912ca3d590af6
sapm2000 <52897462+sapm2000@users.noreply.github.com>
2020-04-03 21:43:09 -0400

    crud

---
+++ CrudElectiva/src/modelo/Conexion.java
@@ -0,0 +1,32 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexion {
+
+    /*Aqui Declaramoslas variables y las inicializamos.*/
+    private final String base = "electiva";
+    private final String user = "postgres";
+    final String clave = "1234";
+    private final String url = "jdbc:postgresql://localhost:5432/" + base;
+    private Connection con = null;
+
+    /*constructor para  obtener la conexion de la base de datos*/
+    public Connection getConexion() {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(this.url, this.user, this.clave);
+        } catch (SQLException e) {
+            System.err.println(e);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+
+    }
+}
