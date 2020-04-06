4ae5ee57f70c24e1dfec4d9cd33fa8e21dfbba3b
Anthony Jhen <ajhensuarez@gmail.com>
2020-04-05 22:55:58 -0400

    1
    1

--- Bitacora/Nueva Carpeta/SGC/src/controlador/controladorCategoriaGastos.java
+++ Bitacora/Nueva Carpeta/SGC/src/controlador/controladorCategoriaGastos.java
@@ -23,6 +23,7 @@ import javax.swing.table.TableRowSorter;
 import modelo.CategoriaGasto;
 import vista.catalogoCategoriaGastos;
 import vista.categoriaGastos;
+import controlador.Validacion;
 
 /**
  *
@@ -49,6 +50,8 @@ public class controladorCategoriaGastos implements ActionListener, MouseListener
         this.catacg.jTable2.addMouseListener(this);
         this.catacg.jTextField1.addKeyListener(this);
         this.catacg.addWindowListener(this);
+        this.cg.txtnombre.addKeyListener(this);
+        this.cg.txtdescripcion.addKeyListener(this);
 
     }
 
@@ -98,9 +101,10 @@ public class controladorCategoriaGastos implements ActionListener, MouseListener
        
 
         if (e.getSource() == cg.btnGuardar) {
+            if(validar()){
             modcg.setNombre(cg.txtnombre.getText());
             modcg.setDescripcion(cg.txtdescripcion.getText());
-           
+            }
 
             if (modcg.registrar(modcg)) {
 
@@ -137,10 +141,11 @@ public class controladorCategoriaGastos implements ActionListener, MouseListener
         }
 
         if (e.getSource() == cg.btnModificar) {
+            if(validar()){
              modcg.setNombre(cg.txtnombre.getText());
              modcg.setDescripcion(cg.txtdescripcion.getText());
               modcg.setId(Integer.parseInt(cg.txtId.getText()));
-             
+            }
              
              
              if (modcg.modificar(modcg)) {
@@ -224,8 +229,18 @@ public class controladorCategoriaGastos implements ActionListener, MouseListener
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped(KeyEvent ke) {
+            if (ke.getSource() == cg.txtnombre) {
+            Validacion.soloLetras(ke);
+            Validacion.Espacio(ke);
+            Validacion.limite(ke, cg.txtnombre.getText(), 8);
+        }
+        if (ke.getSource() == cg.txtdescripcion) {
 
+            Validacion.soloLetras(ke);
+            Validacion.Espacio(ke);
+            Validacion.limite(ke, cg.txtdescripcion.getText(), 20);
+        }
     }
 
     @Override
@@ -271,6 +286,31 @@ public class controladorCategoriaGastos implements ActionListener, MouseListener
     @Override
     public void windowDeactivated(WindowEvent e) {
 
+    }
+         private Boolean validar() {
+
+        Boolean resultado = true;
+        String msj = "";
+
+        if (cg.txtnombre.getText().isEmpty()) {
+
+            msj += "El campo nombre categoria no puede estar vacío\n";
+            resultado = false;
+        }
+        if (cg.txtdescripcion.getText().isEmpty()) {
+
+            msj += "El campo Descripcion no puede estar vacío\n";
+            resultado = false;
+        }
+
+
+
+        if (!resultado) {
+
+            JOptionPane.showMessageDialog(null, msj, "Advertencia", JOptionPane.WARNING_MESSAGE);
+        }
+
+        return resultado;
     }
 
 }
