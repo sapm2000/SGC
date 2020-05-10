3c543f4c7f495470dab06bd89a1260f7bfb4978a
maryosinger05 <maryorithsinger05@gmail.com>
2020-05-10 14:52:47 -0430

    b

--- Bitacora/Nueva Carpeta/SGC/src/vista/buscarProveedor.java
+++ Bitacora/Nueva Carpeta/SGC/src/vista/buscarProveedor.java
@@ -71,6 +71,7 @@ public class buscarProveedor extends javax.swing.JFrame {
         jTextField1 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
